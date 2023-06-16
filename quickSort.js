576beb10e762ea3db6e5c8c99c5cae18ac930302
r134x7 <r134x7@gmx.com>
2023-06-17 07:37:24 +1000

    recursive quick sort in javascript

---
+++ quickSort.js
@@ -0,0 +1,35 @@
+// Soure of original solution: https://stackoverflow.com/questions/5185864/javascript-quicksort
+/*
+    Spent time improving on the original solution found as the Space complexity given by "Benny Code" was not optimal.
+*/
+function quickSort3(list) {
+
+    if (list.length <= 1) {
+        return list
+    }
+
+    function subList(list, index, pivot, accumulator, condition) {
+        if (list[index] === undefined) {
+            return accumulator
+        } else if ((list[index] < pivot && condition === "lt") || (list[index] > pivot && condition === "gt")) {
+            return subList(list, index + 1, pivot, accumulator.concat(list[index]), condition)
+        } else {
+            return subList(list, index + 1, pivot, accumulator, condition)
+        }
+    }
+
+    return quickSort3(
+        subList(list, 0, list[Math.floor(list.length / 2)], [], "lt")
+    ).concat(list[Math.floor(list.length / 2)], quickSort3(
+        subList(list, 0, list[Math.floor(list.length / 2)], [], "gt")
+    ));
+};
+
+  const unsorted = [];
+  for (let i = 0; i < 20; i++) {
+    unsorted.push(Math.round(Math.random() * 2000));
+  }
+
+  const sorted = quickSort3(unsorted);
+  
+  console.log(sorted);
