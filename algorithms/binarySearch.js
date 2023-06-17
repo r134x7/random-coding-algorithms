accab7171d2e8194a857417e025c62d4f39106c9
r134x7 <r134x7@gmx.com>
2023-06-18 05:31:27 +1000

    added while loop and performance tests

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,4 +1,4 @@
-function binarySearch(list, searchValue, indexStart, indexEnd) {
+function binarySearchRecursive(list, searchValue, indexStart, indexEnd) {
     var indexMiddle = Math.floor((indexStart + indexEnd) / 2);
     if (indexStart > indexEnd) {
         return -1;
@@ -7,12 +7,37 @@ function binarySearch(list, searchValue, indexStart, indexEnd) {
         return indexMiddle;
     }
     else if (list[indexMiddle] > searchValue) {
-        return binarySearch(list, searchValue, indexStart, indexMiddle - 1);
+        return binarySearchRecursive(list, searchValue, indexStart, indexMiddle - 1);
     }
     else {
-        return binarySearch(list, searchValue, indexMiddle + 1, indexEnd);
+        return binarySearchRecursive(list, searchValue, indexMiddle + 1, indexEnd);
     }
 }
+function binarySearchLoop(list, target) {
+    var start = 0;
+    var end = list.length - 1;
+    while (start <= end) {
+        var middle = Math.floor((start + end) / 2);
+        if (list[middle] === target) {
+            return middle;
+        }
+        else if (list[middle] < target) {
+            start = middle + 1;
+        }
+        else {
+            end = middle - 1;
+        }
+    }
+    return -1;
+}
 var useList = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-var sortedListB = binarySearch(useList, Number(process.argv[2]), 0, useList.length - 1);
-console.log(sortedListB);
+var a0 = performance.now();
+var sortedListR = binarySearchRecursive(useList, Number(process.argv[2]), 0, useList.length - 1);
+var a1 = performance.now();
+var a2 = performance.now();
+var sortedListL = binarySearchLoop(useList, Number(process.argv[2]));
+var a3 = performance.now();
+console.log(sortedListR);
+console.log("Recursion time: ".concat(a1 - a0, " milliseconds."));
+console.log(sortedListL);
+console.log("Loop time: ".concat(a3 - a2, " milliseconds."));
--- algorithms/binarySearch.ts
+++ algorithms/binarySearch.ts
@@ -1,4 +1,4 @@
-function binarySearch(list: number[], searchValue: number, indexStart: number, indexEnd: number) {
+function binarySearchRecursive(list: number[], searchValue: number, indexStart: number, indexEnd: number) {
 
     let indexMiddle = Math.floor((indexStart + indexEnd) / 2);
 
@@ -9,20 +9,53 @@ function binarySearch(list: number[], searchValue: number, indexStart: number, i
       return indexMiddle
     } else if (list[indexMiddle] > searchValue) {
 
-      return binarySearch(list, searchValue, indexStart, indexMiddle-1)
+      return binarySearchRecursive(list, searchValue, indexStart, indexMiddle-1)
     } else {
 
-      return binarySearch(list, searchValue, indexMiddle+1, indexEnd)
+      return binarySearchRecursive(list, searchValue, indexMiddle+1, indexEnd)
     }
+}
+
+function binarySearchLoop(list: number[], target: number) {
+
+    let start = 0;
+    let end = list.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (list[middle] === target) {
+            return middle;
+        } else if (list[middle] < target) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
+    return -1;
 }
 
 const useList = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
-const sortedListB = binarySearch(
+const a0 = performance.now();
+const sortedListR = binarySearchRecursive(
     useList,
     Number(process.argv[2]),
     0,
     useList.length-1
 )
+const a1 = performance.now();
 
-console.log(sortedListB)
+const a2 = performance.now();
+const sortedListL = binarySearchLoop(
+    useList,
+    Number(process.argv[2]),
+)
+const a3 = performance.now();
+
+console.log(sortedListR)
+console.log(`Recursion time: ${a1 - a0} milliseconds.`);
+
+console.log(sortedListL)
+console.log(`Loop time: ${a3 - a2} milliseconds.`);
