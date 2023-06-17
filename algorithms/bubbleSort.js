f54ac58a3750b9d482e55af09db6b0c0a1d486b0
r134x7 <r134x7@gmx.com>
2023-06-18 07:22:50 +1000

    added bubble sort loop and performance tests

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var randomFunctions_1 = require("../functions/randomFunctions");
-function bubbleSort3(list) {
-    console.log(list);
+function bubbleSortRecursive(list) {
     function swap(list, index, accumulator, sortCount) {
         if (list[index] === undefined) {
             return (sortCount === 0)
@@ -15,5 +14,47 @@ function bubbleSort3(list) {
     }
     return swap(list, 0, [], 0);
 }
-var sortedList = bubbleSort3((0, randomFunctions_1.listGenerator)(20, 100));
-console.log(sortedList);
+function bubbleSortLoop(list) {
+    var index = 0;
+    var sorted = false;
+    var sortCount = 0;
+    var sortedList = list;
+    var sorting = [];
+    while (sorted === false) {
+        if (index > list.length - 1) {
+            if (sortCount === 0) {
+                sortedList = sorting;
+                sorted = true;
+                // prevents loop from continuing current loop which would push an undefined value to list
+                break;
+            }
+            else {
+                sortCount = 0;
+                index = 0;
+                sortedList = sorting;
+                sorting = [];
+            }
+        }
+        if (sortedList[index] > sortedList[index + 1] && sortedList[index + 1] !== undefined) {
+            sorting.push(sortedList[index + 1], sortedList[index]);
+            index = index + 2;
+            sortCount++;
+        }
+        else {
+            sorting.push(sortedList[index]);
+            index++;
+        }
+    }
+    return sortedList;
+}
+var generateList = (0, randomFunctions_1.listGenerator)(20, 100);
+var b0 = performance.now();
+var sortedListR = bubbleSortRecursive(generateList);
+var b1 = performance.now();
+var b2 = performance.now();
+var sortedListL = bubbleSortLoop(generateList);
+var b3 = performance.now();
+console.log(sortedListR);
+console.log("Recursion time: ".concat(b1 - b0, " milliseconds."));
+console.log(sortedListL);
+console.log("Loop time: ".concat(b3 - b2, " milliseconds."));
--- algorithms/bubbleSort.ts
+++ algorithms/bubbleSort.ts
@@ -1,7 +1,7 @@
 import { listGenerator } from "../functions/randomFunctions";
 
-function bubbleSort3(list: number[]) {
-    console.log(list)
+function bubbleSortRecursive(list: number[]) {
+
     function swap(list: number[], index: number, accumulator: number[], sortCount: number): number[] {
 
         if (list[index] === undefined) {
@@ -18,6 +18,55 @@ function bubbleSort3(list: number[]) {
     return swap(list, 0, [], 0)
 }
 
-const sortedList = bubbleSort3(listGenerator(20, 100));
+function bubbleSortLoop(list: number[]) {
+
+    let index = 0;
+    let sorted = false;
+    let sortCount = 0;
+    let sortedList: number[] = list;
+    let sorting: number[] = [];
+
+    while (sorted === false) {
+
+        if (index > list.length - 1) {
+            if (sortCount === 0) {
+                sortedList = sorting;
+                sorted = true;
+                // prevents loop from continuing current loop which would push an undefined value to list
+                break
+            } else {
+                sortCount = 0;
+                index = 0;
+                sortedList = sorting;
+                sorting = [];
+            }
+        }
+
+        if (sortedList[index] > sortedList[index+1] && sortedList[index+1] !== undefined) {
+            sorting.push(sortedList[index+1], sortedList[index]);
+            index = index + 2;
+            sortCount++
+        } else {
+            sorting.push(sortedList[index]);
+            index++;
+        }
+    }
+
+    return sortedList
+}
+
+const generateList = listGenerator(20, 100);
+
+const b0 = performance.now();
+const sortedListR = bubbleSortRecursive(generateList);
+const b1 = performance.now();
+
+const b2 = performance.now();
+const sortedListL = bubbleSortLoop(generateList);
+const b3 = performance.now();
   
-console.log(sortedList);
+console.log(sortedListR)
+console.log(`Recursion time: ${b1 - b0} milliseconds.`);
+
+console.log(sortedListL)
+console.log(`Loop time: ${b3 - b2} milliseconds.`);
