632f461651b6e4550fdb095a8752897290818ac6
r134x7 <r134x7@gmx.com>
2023-06-17 20:06:00 +1000

    completed recursive bubblesort algorithm

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -2,15 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var randomFunctions_1 = require("../functions/randomFunctions");
 function bubbleSort3(list) {
-    function swap(list, index, accumulator) {
+    console.log(list);
+    function swap(list, index, accumulator, sortCount) {
         if (list[index] === undefined) {
-            return accumulator;
+            return (sortCount === 0)
+                ? accumulator
+                : swap(accumulator, 0, [], 0);
         }
         return (list[index] > list[index + 1])
-            ? swap(list, index + 1, accumulator.concat(list[index + 1], list[index]))
-            : swap(list, index + 1, accumulator.concat(list[index], list[index + 1]));
+            ? swap(list, index + 2, accumulator.concat(list[index + 1], list[index]), sortCount + 1)
+            : swap(list, index + 1, accumulator.concat(list[index]), sortCount);
     }
-    return swap(list, 0, []);
+    return swap(list, 0, [], 0);
 }
 var sortedList = bubbleSort3((0, randomFunctions_1.listGenerator)(20, 100));
 console.log(sortedList);
--- algorithms/bubbleSort.ts
+++ algorithms/bubbleSort.ts
@@ -1,19 +1,21 @@
 import { listGenerator } from "../functions/randomFunctions";
 
 function bubbleSort3(list: number[]) {
-
-    function swap(list: number[], index: number, accumulator: number[]): number[] {
+    console.log(list)
+    function swap(list: number[], index: number, accumulator: number[], sortCount: number): number[] {
 
         if (list[index] === undefined) {
-            return accumulator
-        }
+            return (sortCount === 0)
+                ? accumulator 
+                : swap(accumulator, 0, [], 0)
+        } 
 
         return (list[index] > list[index+1])
-            ? swap(list, index + 1, accumulator.concat(list[index + 1], list[index]))
-            : swap(list, index + 1, accumulator.concat(list[index], list[index+1]))
+            ? swap(list, index + 2, accumulator.concat(list[index + 1], list[index]), sortCount + 1)
+            : swap(list, index + 1, accumulator.concat(list[index]), sortCount)
     }
 
-    return swap(list, 0, [])
+    return swap(list, 0, [], 0)
 }
 
 const sortedList = bubbleSort3(listGenerator(20, 100));
