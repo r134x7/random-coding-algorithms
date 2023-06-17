1bbb80ae750a9f4a310c6570f9b92e0dee00cf8f
r134x7 <r134x7@gmx.com>
2023-06-17 21:34:12 +1000

    completed for testing

--- algorithms/mergeSort.ts
+++ algorithms/mergeSort.ts
@@ -12,7 +12,7 @@ function mergeSort3(list: number[]): number[] {
         ) 
 }
 
-function mergeLists(listLeft, listRight) {
+function mergeLists(listLeft: number[], listRight: number[]): number[] {
 
     // compare listLeft with listRight
     // 1:1 => if l > r then return [r, l]
@@ -27,6 +27,21 @@ function mergeLists(listLeft, listRight) {
         the left or the right list and the process repeats
     */
 
+    function sortLists(leftList: number[], rightList: number[], leftIndex: number, rightIndex: number, accumulator: number[]): number[] {
+
+        if (leftList[leftIndex] === undefined) {
+            return accumulator.concat(rightList.slice(rightIndex))
+        } else if (rightList[rightIndex] === undefined) {
+            return accumulator.concat(leftList.slice(leftIndex))
+        } else {
+
+            return (leftList[leftIndex] > rightList[rightIndex])
+                ? sortLists(leftList, rightList, leftIndex, rightIndex + 1, accumulator.concat(rightList[rightIndex]))
+                : sortLists(leftList, rightList, leftIndex + 1, rightIndex, accumulator.concat(leftList[leftIndex]))
+        }
+    }
+
+    return sortLists(listLeft, listRight, 0, 0, []);
 }
 
 const sortedList = mergeSort3(listGenerator(20, 2000));
