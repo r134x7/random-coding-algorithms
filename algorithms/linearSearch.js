25bfeb1aa4c051c017b97318357051956db7b847
r134x7 <r134x7@gmx.com>
2023-06-18 05:20:01 +1000

    added performance tests and while loop

--- algorithms/linearSearch.js
+++ algorithms/linearSearch.js
@@ -1,4 +1,4 @@
-function linearSearch(list, target, index) {
+function linearSearchRecursive(list, target, index) {
     if (list[index] === undefined) {
         return -1;
     }
@@ -6,8 +6,28 @@ function linearSearch(list, target, index) {
         return index;
     }
     else {
-        return linearSearch(list, target, index + 1);
+        return linearSearchRecursive(list, target, index + 1);
     }
 }
-var unsortedList = linearSearch([3, 8, 32, 9, 4, -5, 0], Number(process.argv[2]), 0);
-console.log(unsortedList);
+function linearSearchLoop(list, target) {
+    var index = 0;
+    while (list[index] !== undefined) {
+        if (list[index] === target) {
+            return index;
+        }
+        else {
+            index++;
+        }
+    }
+    return -1;
+}
+var t0 = performance.now();
+var unsortedListR = linearSearchRecursive([3, 8, 32, 9, 4, -5, 0], Number(process.argv[2]), 0);
+var t1 = performance.now();
+var t2 = performance.now();
+var unsortedListL = linearSearchLoop([3, 8, 32, 9, 4, -5, 0], Number(process.argv[2]));
+var t3 = performance.now();
+console.log(unsortedListR);
+console.log("Recursion time: ".concat(t1 - t0, " milliseconds."));
+console.log(unsortedListL);
+console.log("Loop time: ".concat(t3 - t2, " milliseconds."));
--- algorithms/linearSearch.ts
+++ algorithms/linearSearch.ts
@@ -1,18 +1,46 @@
-function linearSearch(list: number[], target: number, index: number): number {
+function linearSearchRecursive(list: number[], target: number, index: number): number {
 
     if (list[index] === undefined) {
         return -1;
     } else if (list[index] === target) {
         return index;
     } else {
-        return linearSearch(list, target, index + 1)
+        return linearSearchRecursive(list, target, index + 1)
     }
 }
 
-const unsortedList = linearSearch(
+function linearSearchLoop(list: number[], target: number) {
+
+    let index = 0;
+
+    while (list[index] !== undefined) {
+        if (list[index] === target) {
+            return index;
+        } else {
+            index++
+        }
+    }
+
+    return -1;
+}
+
+const t0 = performance.now();
+const unsortedListR = linearSearchRecursive(
     [3, 8, 32, 9, 4, -5, 0],
     Number(process.argv[2]),
     0
 )
+const t1 = performance.now();
 
-console.log(unsortedList)
+const t2 = performance.now();
+const unsortedListL = linearSearchLoop(
+    [3, 8, 32, 9, 4, -5, 0],
+    Number(process.argv[2])
+)
+const t3 = performance.now();
+
+console.log(unsortedListR)
+console.log(`Recursion time: ${t1 - t0} milliseconds.`);
+
+console.log(unsortedListL)
+console.log(`Loop time: ${t3 - t2} milliseconds.`);
